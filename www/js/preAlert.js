d56c01d0498befc3bb36936a20dd4ad657e74e31
iBorisLtd <mobile@iboris.eu>
2014-09-12 10:11:33 +0300

    PreAlert Layout

---
+++ www/js/preAlert.js
@@ -0,0 +1,17 @@
+﻿PreAlertView = ObservableView.extend({
+    _initView: function () {
+        ObservableView.fn._initView.call(this);
+    },
+
+
+
+});
+
+
+
+$.extend(app, {
+    onPreAlertViewShow: function (e) {
+        app.preAlertView.reset(e.view);
+    },
+    preAlertView: new PreAlertView(app)
+});
