d71592522d55dbcaeea8eafcc9c342a2193e463b
mobile@iboris.eu <mobile@iboris.eu>
2014-09-15 16:03:35 +0300

    flush latest

---
+++ www/js/components/FlashlightWrapper.js
@@ -0,0 +1,31 @@
+﻿function FlashlightWrapper() {
+    var that = this;
+    if (window.window.plugins && window.plugins.flashlight) {
+        this._flashlight = window.plugins.flashlight;
+        this._flashlight.available(function (isAvailable) {
+            that._available = isAvailable;
+        });
+    }
+}
+
+FlashlightWrapper.prototype = {
+
+    toggle: function () {
+        if (this._available) {
+            this._flashlight.toggle();
+        }
+    },
+    switchOn: function(){
+        if (this._available) {
+            this._flashlight.switchOn();
+        }
+    },
+    switchOff: function () {
+        if (this._available) {
+            this._flashlight.switchOff();
+        }
+    },
+
+    _available: false,
+    _flashlight: null,
+};
