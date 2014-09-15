b05cc9d418f9dd4933418cfcb7e83d8e565b3bc6
iBorisLtd <mobile@iboris.eu>
2014-09-15 14:59:57 +0300

    GPS Location Address

--- www/js/app.js
+++ www/js/app.js
@@ -1,179 +1,68 @@
 var app = {
     // Application Constructor
     initialize: function () {
-        this.initKendo();
-        this.bindEvents();
+        this._initKendo();
+        this._bindEvents();
+        this._initPlugins();
     },
-    initKendo: function () {
+
+
+    _initKendo: function () {
         this.kendoApp = new kendo.mobile.Application(document.body, {
             layout: 'default',
             initial: 'views/home.html',
             transition: 'slide'
         });
     },
-    bindEvents: function () {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+    _bindEvents: function () {
+        document.addEventListener('deviceready', this._onDeviceReady, false);
     },
-    onDeviceReady: function () {
+
+    _onDeviceReady: function () {
         navigator.splashscreen.hide();
-        //this._getGeolocation();
-
-    },
-    _getGeolocation: function () {
-        var options = { enableHighAccuracy: true };
-        navigator.geolocation.getCurrentPosition(_getPosition, _positionError, options);
+        this._getGeolocation();
     },
 
-    _getPosition: function (position) {
-        var latitude = position.coords.latitude;
-        var longitude = position.coords.longitude;
-        _reverseGeocode(latitude, longitude);
+    _initPlugins: function () {
+        this.flashlight = new FlashlightWrapper();
+        this.gps = new GPSWrapper();
     },
-
-    _positionError: function () {
-        navigator.notification.alert('Could not find the current location.');
-    },
-
-    _reverseGeocode: function (latitude, longitude) {
-        var reverseGeocoder = new google.maps.Geocoder();
-        var currentPosition = new google.maps.LatLng(latitude, longitude);
-        reverseGeocoder.geocode({ 'latLng': currentPosition }, function (results, status) {
-
-            if (status == google.maps.GeocoderStatus.OK) {
-                if (results[0]) {
-                    var splittedAddress = results[0].formatted_address.split(',');
-                    var formattedAddress = splittedAddress[0] + '<br />' + splittedAddress[1] + '<br />' + splittedAddress[2];
-
-                    this.set('currentAddress', formattedAddress);
-                }
-                else {
-                    navigator.notification.alert('Unable to detect your address.');
-                }
-            } else {
-                navigator.notification.alert('Unable to detect your address.');
-            }
-        });
-    },
-
-    //_getLocation: function (options) {
-    //    var dfd = new $.Deferred();
-
-    //    //Default value for options
-    //    if (options === undefined) {
-    //        options = { enableHighAccuracy: true };
-    //    }
-
-    //    navigator.geolocation.getCurrentPosition(
-    //        function (position) {
-    //            dfd.resolve(position);
-    //        },
-    //        function (error) {
-    //            dfd.reject(error);
-    //        },
-    //        options);
-
-    //    return dfd.promise();
-    //},
-    //_getAddressFromCoordinates: function (position) {
-    //    var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-    //    $.ajax({
-    //        type: 'GET',
-    //        url: 'http://maps.google.com/maps/api/geocode/xml?latlng={0}&sensor=false'.Format(latlng),
-    //        success: function (response) {
-    //            console.log(response);
-    //        },
-    //        error: function (e) {
-    //            var responseObject = $.parseJSON(e.responseText);
-    //            alert(responseObject.Message);
-    //        }
-    //    });
-    //},
-
 
     kendoApp: null,
-    currentAddress: null
+    flashlight: null,
+    gps: null
 };
 
-//function getLocation() {
-//    navigator.geolocation.getCurrentPosition(
-//        function (position) {
-//            console.log(position);
-//            return position;
-//        },
-//        function (error) {
-//            alert(error.message);
-//        });
+//function _getPosition(position) {
+//    var latitude = position.coords.latitude;
+//    var longitude = position.coords.longitude;
+//    _reverseGeocode(latitude, longitude);
 //}
 
-//console.log(getLocation());
-//var position = getLocation();
-////var latitude = position.coords.latitude;
-////var longitude = position.coords.longitude;
+//function _positionError() {
+//    alert('Could not find the current location.');
+//}
 
-//var latitude = 43.204665;
-//var longitude = 27.910542;
-
-//(function () {
-//    var geocoder;
-
-//    geocoder = new google.maps.Geocoder();
-//    var latlng = new google.maps.LatLng(latitude, longitude);
-
-//    geocoder.geocode({ 'latLng': latlng }, function (results, status) {
-//        console.log(results);
+//function _reverseGeocode(latitude, longitude) {
+//    var reverseGeocoder = new google.maps.Geocoder();
+//    var currentPosition = new google.maps.LatLng(latitude, longitude);
+//    reverseGeocoder.geocode({ 'latLng': currentPosition }, function (results, status) {
 //        if (status == google.maps.GeocoderStatus.OK) {
 //            if (results[0]) {
-//                var add = results[0].formatted_address;
-//                var value = add.split(",");
-
-//                count = value.length;
-//                country = value[count - 1];
-//                state = value[count - 2];
-//                city = value[count - 3];
-//                alert("city name is: " + city);
+//                var splittedAddress = results[0].formatted_address.split(',');
+//                var formattedAddress = splittedAddress[0] + '<br />' + splittedAddress[1] + '<br />' + splittedAddress[2];
+//                $('#gps-location-info span').html(formattedAddress);
 //            }
 //            else {
-//                alert("Address not found!");
+//                alert('Unable to detect your address.');
 //            }
-//        }
-//        else {
-//            //document.getElementById("location").innerHTML="Geocoder failed due to: " + status;
-//            //alert("Geocoder failed due to: " + status);
+//        } else {
+//            alert('Unable to detect your address.');
 //        }
 //    });
-//})();
+//}
 
-
-function _getPosition(position) {
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
-    _reverseGeocode(latitude, longitude);
-}
-
-function _positionError() {
-    alert('Could not find the current location.');
-}
-
-function _reverseGeocode(latitude, longitude) {
-    var reverseGeocoder = new google.maps.Geocoder();
-    var currentPosition = new google.maps.LatLng(latitude, longitude);
-    reverseGeocoder.geocode({ 'latLng': currentPosition }, function (results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-            if (results[0]) {
-                var splittedAddress = results[0].formatted_address.split(',');
-                var formattedAddress = splittedAddress[0] + '<br />' + splittedAddress[1] + '<br />' + splittedAddress[2];
-                $('#gps-location-info span').html(formattedAddress);
-            }
-            else {
-                alert('Unable to detect your address.');
-            }
-        } else {
-            alert('Unable to detect your address.');
-        }
-    });
-}
-
-$(function () {
-    var options = { enableHighAccuracy: true };
-    navigator.geolocation.getCurrentPosition(_getPosition, _positionError, options);
-});
+//$(function () {
+//    var options = { enableHighAccuracy: true };
+//    navigator.geolocation.getCurrentPosition(_getPosition, _positionError, options);
+//});
---
+++ www/js/components/FlashlightWrapper.js
@@ -0,0 +1,29 @@
+﻿function FlashlightWrapper() {
+    var that = this;
+    this._flashlight = window.plugins.flashlight;
+    this._flashlight.available(function (isAvailable) {
+        that._available = isAvailable;
+    });
+}
+
+FlashlightWrapper.prototype = {
+
+    toggle: function () {
+        if (this._available) {
+            this._flashlight.toggle();
+        }
+    },
+    switchOn: function(){
+        if (this._available) {
+            this._flashlight.switchOn();
+        }
+    },
+    switchOff: function () {
+        if (this._available) {
+            this._flashlight.switchOff();
+        }
+    },
+
+    _available: null,
+    _flashlight: null,
+};
